293a09ddb31240ebe61a026bd61d8c9049877e6c
LukeBucciarelli <luke.bucciarelli@gmail.com>
2015-12-20 22:08:17 -0700

    1st commit

---
+++ Quiz App/script.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+    
+
+   
+        
+   
+
+
+
+ function quiz() {
+        var scoreAry = [];
+        var questions = [{
+            q: "Q-1: Who was the first person to climb 5.15?"
+            s: ["A.  Royal Robbins","B. Chris Sharma","C. Tommy Caldwell","D.  Fred Nicole" ],
+            a: "B.",
+           
+
+
+
+
+
+
+
+
+
+    });
